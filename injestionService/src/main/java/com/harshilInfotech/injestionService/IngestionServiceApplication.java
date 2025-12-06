c1ea8e9a12bf3fed4b202db144b6fd0e140b5ed5
harshil8705 <harshil8705@users.noreply.github.com>
2025-12-06 11:28:02 +0530

    Refactored Spelling Mistakes:)

--- injestionService/src/main/java/com/harshilInfotech/injestionService/IngestionServiceApplication.java
+++ injestionService/src/main/java/com/harshilInfotech/injestionService/IngestionServiceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InjestionServiceApplication {
+public class IngestionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InjestionServiceApplication.class, args);
+		SpringApplication.run(IngestionServiceApplication.class, args);
 	}
 
 }
+
